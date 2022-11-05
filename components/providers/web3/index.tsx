2d8fc246b0157393af67327acca968a1b1755387
Mauricio <mauricio.tp_@outlook.com>
2022-11-05 18:55:13 -0300

    set window ethereum

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -3,6 +3,7 @@ import {
   FunctionComponent,
   ReactNode,
   useContext,
+  useEffect,
   useState,
 } from 'react';
 
@@ -16,6 +17,19 @@ const Web3Context = createContext<Web3State>(createDefaultState());
 
 const Web3Provider: FunctionComponent<Web3ProviderProps> = ({ children }) => {
   const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+  useEffect(() => {
+    function initWeb3() {
+      setWeb3Api({
+        ethereum: window.ethereum,
+        provider: null,
+        contract: null,
+        isLoading: false,
+      });
+    }
+
+    initWeb3();
+  }, []);
 
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
