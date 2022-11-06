7b9b0e46b20a54d55202b80904259773797db108
Mauricio <mauricio.tp_@outlook.com>
2022-11-06 12:20:23 -0300

    ERC721

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NftMarket {}
+contract NftMarket is ERC721URIStorage {
+    constructor() ERC721("CreaturesNFT", "CNFT") {}
+}
