a5ee3a8f1a481462018afef586bb54a0fc9a01dc
Mauricio <mauricio.tp_@outlook.com>
2022-11-07 14:36:19 -0300

    use account abstraction

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -5,7 +5,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import ActiveLink from '../link';
-import { useWeb3 } from '@providers/web3';
+import { useAccount } from '@hooks/web3';
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -17,10 +17,9 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
-  const { hooks } = useWeb3();
-  const { data } = hooks.useAccount('Hi there');
+  const { account } = useAccount();
 
-  console.log(data);
+  console.log(account.data);
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
