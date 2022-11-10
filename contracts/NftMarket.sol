491250128c95bdcbe76fa4ea66b1d2ec409bf073
Mauricio <mauricio.tp_@outlook.com>
2022-11-10 13:10:28 -0300

    mint token

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _listedItems;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("CreaturesNFT", "CNFT") {}
+
+    function mintToken(string memory tokenURI)
+        public
+        payable
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        _listedItems.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
 }
