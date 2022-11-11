a672f4dad2288b0a8df5a326623613b35cc22787
Mauricio <mauricio.tp_@outlook.com>
2022-11-11 16:00:33 -0300

    check if tokenUri exists

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -9,6 +9,8 @@ contract NftMarket is ERC721URIStorage {
     Counters.Counter private _listedItems;
     Counters.Counter private _tokenIds;
 
+    mapping(string => bool) private _usedTokenURIs;
+
     constructor() ERC721("CreaturesNFT", "CNFT") {}
 
     function mintToken(string memory tokenURI)
@@ -16,6 +18,8 @@ contract NftMarket is ERC721URIStorage {
         payable
         returns (uint256)
     {
+        require(!tokenURIExists(tokenURI), "Token URI already exists");
+
         _tokenIds.increment();
         _listedItems.increment();
 
@@ -23,7 +27,12 @@ contract NftMarket is ERC721URIStorage {
 
         _safeMint(msg.sender, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
+        _usedTokenURIs[tokenURI] = true;
 
         return newTokenId;
+    }
+
+    function tokenURIExists(string memory tokenURI) public view returns (bool) {
+        return _usedTokenURIs[tokenURI] == true;
     }
 }
